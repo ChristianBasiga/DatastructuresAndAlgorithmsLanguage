f2925dd32f8d79f47d0bd95ff9a4fcda980b2097
Prince Christian Basiga <princebasiga@gmail.com>
2018-03-13 14:36:38 -0700

    Got rid of NodeMenuClass and reworked structure of NodeOption Prefabs.

--- DS&A_LanguageAndVisual/Assets/scripts/UserInterface/VisualNodeOption.cs
+++ DS&A_LanguageAndVisual/Assets/scripts/UserInterface/VisualNodeOption.cs
@@ -2,48 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 //These are the objects placed in the menu. They'll have a refernce to actual VisualNodeOption using.
 //and when create is clicked will instantiate what this is referring to. Fucking Decorator yo. I think.
 public class VisualNodeOption : MonoBehaviour {
 
-    //This will be the actual node this option is referencing.
-    //The gameobjects themselves will be carbon copies of VisualNode prefabs
-    //could just make this string later on cause of pool, so no more extra to do here.
-    //Actually not string, cause this needs to exist not just as string because will have default values to be preview
+
     public VisualNode referencing;
     public int optionNumber;
+    //Panel containing preview and create buttons
+    public GameObject actionPanel;
+
+    public Button menuButton;
+
 
     void Start()
     {
 
-        //Setting example values for referencing...Might just set this via inspector instead. Taking the prefabs of actual ones, yeah.
-
         //Setting up the buttons.
-        Button previewButton = transform.GetChild(0).GetComponent<Button>();
-        Button createButton = transform.GetChild(1).GetComponent<Button>();
+        Button previewButton = actionPanel.transform.GetChild(0).GetComponent<Button>();
+        Button createButton = actionPanel.transform.GetChild(1).GetComponent<Button>();
+        Button closeButton = actionPanel.transform.GetChild(2).GetComponent<Button>();
 
-        //Menu should now just hold the VisualNodeOptions, and these objects deal with
-        //opening previews and creating the nodes.
+        previewButton.onClick.AddListener(openPreview);
 
-        //This is more logically sound, and each option has own set of buttons so will only one copy of these methods for each onClick on the buttons yo.
-        previewButton.onClick.AddListener((int optionIndex) => {
-            if (optionIndex == optionNumber)
-                openPreview();
-        });
-        
-        createButton.onClick.AddListener((int optionIndex) => {
-            if (optionIndex == optionNumber)
-                menu.createVisualNode(optionNumber);
-        });
+        createButton.onClick.AddListener(createVisualNode);
+
+        closeButton.onClick.AddListener(() => { actionPanel.SetActive(false); });
+
+
+        actionPanel.SetActive(false);
+
+        menuButton.onClick.AddListener(() => { actionPanel.SetActive(true); });
 
         referencing.gameObject.SetActive(false);
     }
 
-    //Actually pulls from pool / instantiates the visualNode this option is referring to.
+   
+
+
     void createVisualNode()
     {
-        
+        GameObject node = Instantiate(referencing, Vector3.zero, Quaternion.identity).gameObject;
+
+        //Not at same position but just at "origin" of canvas probably.
+        node.transform.position = Vector3.zero;//Will set it later
+        //It needs head, and tail stuff, ut don't think actually needs head to contain all that shiz.
+        //node.transform.GetChild(0).gameObject.SetActive(true);
+        node.SetActive(true);
     }
 
     void openPreview()
