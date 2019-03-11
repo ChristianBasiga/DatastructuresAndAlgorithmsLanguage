64cffdf42a8a654cca766cc72f832111122505b0
Prince Christian Basiga <princebasiga@gmail.com>
2019-03-10 21:49:54 -0700

    Cleared old unity project.

--- VisualQuickieCode/Assets/scripts/Commands/Interfaces/IExecute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IExecute
-{
-    void execute(DataStructureLanguage.Syntax.Util.SyntaxTree syntaxTree);
-}
