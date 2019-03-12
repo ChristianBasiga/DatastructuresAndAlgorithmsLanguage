9989dc0daa2ed28d6ad8de524b6c1ee7d4baa801
Prince Christian Basiga <princebasiga@gmail.com>
2019-03-11 20:34:22 -0700

    Implemented conditional statement, updated base statements accordingly

--- visual_quickie_code/lib/model/statements.dart
+++ visual_quickie_code/lib/model/statements.dart
@@ -35,7 +35,13 @@ abstract class StatementNode{
   }
 
   StatementNode get next{
+    onEndExecuting();
     return _next;
+  }
+
+  void setNext(StatementNode toSet){
+
+    _next = toSet;
   }
 
 
@@ -63,8 +69,31 @@ abstract class StatementNode{
 abstract class BlockStatement extends StatementNode{
 
   StatementNode body;
+
+  //Continue block this block is in.
+  StatementNode continueBlock;
   
   BlockStatement(int id) :super(id);
+
+//After return, want to set _return to.
+  @override
+  StatementNode get next{
+
+    onEndExecuting();
+    if (status.executed){
+      return continueBlock;
+    }
+    
+    return _next;
+  }
+
+  @override
+  void setNext(StatementNode toSet){
+
+    continueBlock = toSet;
+    super.setNext(toSet);
+  }
+
 
   void setBody(StatementNode body){
 
@@ -78,12 +107,14 @@ abstract class BlockStatement extends StatementNode{
     //not making this next of it's inner body but it's most outer next.
     //Maybe this process be done prior? When constructing the list from bloc since the function statement
     //will have been already been made, but putting here makes it clear.
-    while (current.next != null){
+    while (current._next != null){
 
       current = current._next;
     }
 
-    current._next = this;
+    //To make sure the reference to the loopback isn't lost for example in block statements
+    //_next could be overwriten by body and envers set to continue block.
+    current.setNext(this);
   }
 }
 
@@ -94,7 +125,7 @@ class FunctionStatement extends BlockStatement{
   //Hmm maybe parameters should be part of environment
   //The reason I say this is parameters should be part of the scope of inner blocks? Maybe their own environment nested here?
   Environment _parameters;
-  StatementNode _returnTo;
+
   FunctionStatement(int id, Environment parameters) :super(id){
 
     _parameters = parameters;
@@ -104,11 +135,95 @@ class FunctionStatement extends BlockStatement{
     //So in scope.
   }
 
+  
+  
+  //Basically assigns what next means.
   void exec(){
 
-    //Exec of function statement means virtually nothing.
-    _returnTo = _next;
     _next = body;
+  }
+}
+
+//Honestly this exact same structure could be used for all, only difference is I reset it at next.
+class ConditionalStatement extends BlockStatement{
+
+
+  bool isLoop;
+  BlockStatement elseNext;
+  ExpressionNode conditionalExpression;
+  ConditionalStatement(int id) :super(id){
+
+    
+    //Setting environment of functions must wrap around parameters
+    //parameters should have same as local, not be after global.
+    //So in scope.
+  }
+
+  //Could be else if.
+  //Once again ideally, I set the loop prior to setting here
+  //also would reduce the duplicate code of body and this.
+  ///BIG NOTE HERE TO DO THAT WHEN START DOING BLOCS / CONTROLLERS.
+  void setElse(BlockStatement elseStatement){
+
+    elseNext = elseStatement;
+    //Same process as setting body, just make this loop back here.
+
+    StatementNode current = elseNext;
+    
+    //Not accessing getter becaues at outermost layer I want next not inner
+    //using private member that I can access so that I don't set next of wrong statement.
+    //Ideally use protected but Dart doesn't have that.
+    while (current._next != null){
+
+        current = current._next;
+    }
+
+    //Same reason as above, make it so loopback is this, review on paper later.
+    current.setNext(this);
+  }
+
+
+  @override
+  StatementNode get next{
+    onEndExecuting();
+    if (status.executed){
+
+      if (isLoop){
+        //doing within here, makes it so I don't gotta do ugly and expensive type in processor.
+        exec();
+        return _next;
+      }
+      else{
+
+        return continueBlock;
+      }
+    }
+    else{
+
+      return _next;
+    }
+
+
+  }
+
+  void exec(){
+
+    onBeginExecuting();
+    VariableNode condition = conditionalExpression.exec(env);
+
+    if (condition.data == "true"){
+
+      _next = body;
+    }
+    else if (elseNext != null){
+
+      //Then when this is returned by next, and its exec gets called, if it is else if
+      //then repeats this process for that node, if it is just a block, then just goes through body.
+      _next = elseNext;
+    }
+    else{
+      _next = continueBlock;
+    }
   }
 }
 
