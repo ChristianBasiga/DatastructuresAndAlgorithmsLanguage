1294ad84428b6a670651502c5c561f70cbd161eb
Prince Christian Basiga <princebasiga@gmail.com>
2019-03-11 19:49:20 -0700

    Updated environment

--- visual_quickie_code/lib/model/environment.dart
+++ visual_quickie_code/lib/model/environment.dart
@@ -1,3 +1,5 @@
+library environment;
+
 import 'dart:collection';
 
 
@@ -16,18 +18,23 @@ class Environment{
  //So building out a BST might be better like original plan.
  //for now doing pseudo code but since I want ref of variable will do that.
  //Actually map is prob fine, Dart doesn't make reinventing the wheel like this very easy.
-   HashMap<String, VariableNode> variables;
+   HashMap<String, VariableNode> _variables;
 
    Environment(){
 
-    variables = HashMap<String, VariableNode>();
+    _variables = HashMap<String, VariableNode>();
    }
 
    VariableNode findVar(String identifier){
 
-      VariableNode var = (variables.containsKey(identifier))? variables[identifier] : null;
+      VariableNode variable = (_variables.containsKey(identifier))? _variables[identifier] : null;
 
-      return var;  
+      return variable;  
+   }
+
+   void add(String identifier, VariableNode value){
+
+     _variables[identifier] = value;
    }
 
 }
@@ -39,20 +46,29 @@ class LocalEnvironment extends Environment {
 
   Environment decorating;
 
+
+  @override
   VariableNode findVar(String identifier){
 
-    VariableNode var = super.findVar(identifier);
+    VariableNode variable = super.findVar(identifier);
     //Will recursively go up tree, if didn't find in variables.
 
     //See if identifier in this instance, return if is.
 
     //Otherwise go up tree, ideally root is base Environment, but incase it's local and decorating is null.
-    if (var == null && decorating != null){
-      var = decorating.findVar(identifier);
+    if (variable == null && decorating != null){
+      variable = decorating.findVar(identifier);
     }
 
-    return var;
+    return variable;
   }
 }
 
+
+class FunctionEnvironment extends LocalEnvironment{
+
+  HashMap<String, VariableNode> parameters;
+  String name;
+
+}
 
